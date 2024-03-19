a904b9fec5651a31f695349cd26d2c169fc8e061
JessieGodden <jgodden@oswego.edu>
2024-03-19 11:07:43 -0400

    Updated Filters page

--- Filters/src/MultiFilters.js
+++ Filters/src/MultiFilters.js
@@ -8,13 +8,30 @@ export default function MultiFilters() {
 
   // Category button states
   const [selectedFilters, setSelectedFilters] = useState([]);
-  const [filteredItems, setFilteredItems] = useState(items);
 
   // Dropdown states
-  const [selectedOption, setSelectedOption] = useState([]);
+  const [selectedSex, setSelectedSex] = useState(null);
+  const [selectedColor, setSelectedColor] = useState(null);
+  const [selectedBreed, setSelectedBreed] = useState(null);
+  const [selectedAge, setSelectedAge] = useState(null);
+
+  // Filtered items states
+  const [filteredItems, setFilteredItems] = useState(items);
 
   // Category options
   let filters = ["Dog", "Cat", "Other"];
+
+  // Dropdown options
+  const uniqueItems = (key) => {
+    if (selectedFilters.length === 0) {
+      const uniqueSet = new Set(items.flatMap((item) => item[key]).flat());
+      return [...uniqueSet].map((value) => ({ value, label: value }));
+    } else {
+      let filteredItems = items.filter(item => selectedFilters.includes(item.category));
+      const uniqueSet = new Set(filteredItems.flatMap((item) => item[key]).flat());
+      return [...uniqueSet].map((value) => ({ value, label: value }));
+    }
+  };
 
   // Handles changing category buttons
   const handleFilterButtonClick = (selectedCategory) => {
@@ -26,28 +43,65 @@ export default function MultiFilters() {
     }
   };
 
-  // Handles changing dropdown options
-  const handleChange = (option) => {
-    setSelectedOption(option)
-    console.log(option)
+  // Handle dropdown options changing
+  const handleChangeSex = (selectedOption) => {
+    setSelectedSex(selectedOption);
+  };
+  
+  const handleChangeColor = (selectedOption) => {
+    setSelectedColor(selectedOption);
+  };
+  
+  const handleChangeBreed = (selectedOption) => {
+    setSelectedBreed(selectedOption);
+  };
+  
+  const handleChangeAge = (selectedOption) => {
+    setSelectedAge(selectedOption);
+  };
+
+  // Filter items based on categories and dropdowns
+  const filterItems = () => {
+    if (selectedFilters.length > 0) {
+      let tempItems = selectedFilters.map((selectedCategory) => {
+        let temp = items.filter((item) => item.category === selectedCategory);
+        if (selectedSex && selectedSex.length > 0) {
+          temp = temp.filter((item) => selectedSex.some((sex) => item.sex.includes(sex.value)));
+        }
+        if (selectedColor && selectedColor.length > 0) {
+          temp = temp.filter((item) => selectedColor.some((color) => item.color.includes(color.value)));
+        }
+        if (selectedBreed && selectedBreed.length > 0) {
+          temp = temp.filter((item) => selectedBreed.some((breed) => item.breed.includes(breed.value)));
+        }
+        if (selectedAge && selectedAge.length > 0) {
+          temp = temp.filter((item) => selectedAge.some((age) => item.age.includes(age.value)));
+        }
+        return temp;
+      });
+      setFilteredItems(tempItems.flat());
+    } else {
+      let tempItems = [...items];
+      if (selectedSex && selectedSex.length > 0) {
+        tempItems = tempItems.filter((item) => selectedSex.some((sex) => item.sex.includes(sex.value)));
+      }
+      if (selectedColor && selectedColor.length > 0) {
+        tempItems = tempItems.filter((item) => selectedColor.some((color) => item.color.includes(color.value)));
+      }
+      if (selectedBreed && selectedBreed.length > 0) {
+        tempItems = tempItems.filter((item) => selectedBreed.some((breed) => item.breed.includes(breed.value)));
+      }
+      if (selectedAge && selectedAge.length > 0) {
+        tempItems = tempItems.filter((item) => selectedAge.some((age) => item.age.includes(age.value)));
+      }
+      setFilteredItems(tempItems);
+    }
   };
 
   useEffect(() => {
     filterItems();
     // eslint-disable-next-line
-  }, [selectedFilters]);
-
-  const filterItems = () => {
-    if (selectedFilters.length > 0) {
-      let tempItems = selectedFilters.map((selectedCategory) => {
-        let temp = items.filter((item) => item.category === selectedCategory);
-        return temp;
-      });
-      setFilteredItems(tempItems.flat());
-    } else {
-      setFilteredItems([...items]);
-    }
-  };
+  }, [selectedFilters, selectedSex, selectedColor, selectedBreed, selectedAge]);
 
   return (
     <div>
@@ -68,12 +122,11 @@ export default function MultiFilters() {
       <div className="dropdowns-container">
         <Select
           placeholder = {'Sex'}
-          options={items}
+          value={selectedSex}
+          options={uniqueItems("sex")}
+          onChange={handleChangeSex}
           isMulti
           className='dropdown'
-          onChange={handleChange}
-          getOptionLabel={(option) => option.sex}
-          getOptionValue={(option) => option.sex}
           theme={(theme) => ({
             ...theme,
             borderRadius: 5,
@@ -87,12 +140,11 @@ export default function MultiFilters() {
 
         <Select
           placeholder = {'Color'}
-          options={items}
+          value={selectedColor}
+          options={uniqueItems("color")}
+          onChange={handleChangeColor}
           isMulti
           className='dropdown'
-          onChange={handleChange}
-          getOptionLabel={(option) => option.color}
-          getOptionValue={(option) => option.color}
           theme={(theme) => ({
             ...theme,
             borderRadius: 5,
@@ -106,12 +158,11 @@ export default function MultiFilters() {
 
         <Select
           placeholder = {'Breed'}
-          options={items}
+          value={selectedBreed}
+          options={uniqueItems("breed")}
+          onChange={handleChangeBreed}
           isMulti
           className='dropdown'
-          onChange={handleChange}
-          getOptionLabel={(option) => option.breed}
-          getOptionValue={(option) => option.breed}
           theme={(theme) => ({
             ...theme,
             borderRadius: 5,
@@ -125,12 +176,11 @@ export default function MultiFilters() {
 
         <Select
           placeholder = {'Age'}
-          options={items}
+          value={selectedAge}
+          options={uniqueItems("age")}
+          onChange={handleChangeAge}
           isMulti
           className='dropdown'
-          onChange={handleChange}
-          getOptionLabel={(option) => option.age}
-          getOptionValue={(option) => option.age}
           theme={(theme) => ({
             ...theme,
             borderRadius: 5,
@@ -147,7 +197,7 @@ export default function MultiFilters() {
         {filteredItems.map((item, idx) => (
           <div key={`items-${idx}`} className="button">
             <p className="name">{item.name}</p>
-            <img className="image" src= {item.image}/>
+            <img className="image" src= {item.image} alt ="Null"/>
             <p className="category">{item.category}</p>
             <p className="sex">{item.sex}</p>
             <p className="color">{item.color}</p>
