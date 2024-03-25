a35481fba8d04b5334ed24a7e882f3adb50c504b
Param <paramrajguru@Params-MacBook-Pro.local>
2024-03-25 18:29:27 -0400

    Added onboarding page

---
+++ onboarding-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Onboarding from './components/Onboarding';
+
+function App() {
+  return (
+    <div className="App">
+      <Onboarding />
+    </div>
+  );
+}
+
+export default App;
---
+++ onboarding-app/src/components/Step2.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import InputMask from 'react-input-mask';
+
+const Step2 = ({ nextStep, prevStep, formData, setForm }) => {
+  const { streetAddress, city, state, zipcode, contactPhone } = formData;
+  const [phoneError, setPhoneError] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...formData, [name]: value });
+
+    // Check for phone number validation
+    if (name === 'contactPhone') {
+      const phoneRegex = /^\+1 \(\d{3}\) \d{3}-\d{4}$/;
+      setPhoneError(!phoneRegex.test(value));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!phoneError) {
+      nextStep();
+    } else {
+      console.log('Please enter a valid phone number');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="signup-form">
+      <label htmlFor="streetAddress">Address:</label>
+      <input
+        type="text"
+        name="streetAddress"
+        id="streetAddress"
+        value={streetAddress}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="city">City:</label>
+      <input
+        type="text"
+        name="city"
+        id="city"
+        value={city}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="state">State:</label>
+      <input
+        type="text"
+        name="state"
+        id="state"
+        value={state}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="zipcode">Zipcode:</label>
+      <input
+        type="number"
+        name="zipcode"
+        id="zipcode"
+        value={zipcode}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="contactPhone">Phone Number:</label>
+      <InputMask
+        mask="+1 (999) 999-9999"
+        maskChar="_"
+        type="tel"
+        name="contactPhone"
+        id="contactPhone"
+        value={contactPhone}
+        onChange={handleChange}
+        required
+      />
+      {phoneError && (
+        <p style={{ color: 'red', fontSize: '0.8em', marginTop: '5px' }}>
+          Please enter a valid phone number in the format +1 (123) 456-7890
+        </p>
+      )}
+
+      <div>
+        <button onClick={prevStep}>Back</button>
+        <button type="submit">Next</button>
+      </div>
+    </form>
+  );
+};
+
+export default Step2;
