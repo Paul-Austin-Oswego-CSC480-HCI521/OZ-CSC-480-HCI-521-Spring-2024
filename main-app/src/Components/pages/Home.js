31759159b1a47d577a6ec7dcebf36675afc0b229
Param <paramrajguru@Params-MacBook-Pro.local>
2024-04-09 12:21:26 -0400

    added css to adoption faqs

--- main-app/src/Components/pages/Home.js
+++ main-app/src/Components/pages/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import BannerBackground from "../../Assets/Hero2.png";
-import dogImage from '../../Assets/dog.png';
-import catImage from '../../Assets/cat.png';
-import birdImage from '../../Assets/bird.png';
-import bunnyImage from '../../Assets/bunny.png';
+import dogImage from "../../Assets/dog.png";
+import catImage from "../../Assets/cat.png";
+import birdImage from "../../Assets/bird.png";
+import bunnyImage from "../../Assets/bunny.png";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Luke from "../../Assets/Furr-Ever friends near you_1.png";
@@ -18,10 +18,9 @@ import Danii from "../../Assets/Screenshot2013.JPG";
 import Slider from "react-slick";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import FAQs from './AdoptionFAQ';
-import Background from '../../Assets/BG pattern.png';
+import FAQs from "./AdoptionFAQ";
+import Background from "../../Assets/BG pattern.png";
 import { useState } from "react";
-
 
 export const Home = () => {
   const scrollToHowItWorks = (e) => {
@@ -33,108 +32,107 @@ export const Home = () => {
     }
   };
 
-    const [zipCode, setZipCode] = useState('13126'); // Define zipCode state
-  
-    const handleZipCodeChange = (e) => {
-      setZipCode(e.target.value);
-    };
+  const [zipCode, setZipCode] = useState("13126"); // Define zipCode state
+
+  const handleZipCodeChange = (e) => {
+    setZipCode(e.target.value);
+  };
 
   const handleSubmitZipCode = (e) => {
     e.preventDefault();
-    
+
     console.log("Hello World");
   };
 
   const teams = [
     {
-    name: "",
-    members: [
+      name: "",
+      members: [
         {
-            name: "Smoothie, M",
-            image: Luke,
-            info: "American Shorthair"
+          name: "Smoothie, M",
+          image: Luke,
+          info: "American Shorthair",
         },
         {
-            name: "Watson, M",
-            image: Finn,
-            info: "American Shorthair"
+          name: "Watson, M",
+          image: Finn,
+          info: "American Shorthair",
         },
         {
-            name: "Victor, M",
-            image: Brownie,
-            info: "American Shorthair"
+          name: "Victor, M",
+          image: Brownie,
+          info: "American Shorthair",
         },
         {
-            name: "Aofie, F",
-            image: Jaxon,
-            info: "American Shorthair"
+          name: "Aofie, F",
+          image: Jaxon,
+          info: "American Shorthair",
         },
         {
-            name: "Smoothie F",
-            image: Chloe,
-            info: "American Shorthair"
+          name: "Smoothie F",
+          image: Chloe,
+          info: "American Shorthair",
         },
         {
-            name: "Jerry",
-            image: Chloe,
-            info: "American Shorthair"
+          name: "Jerry",
+          image: Chloe,
+          info: "American Shorthair",
         },
         {
           name: "Jerry",
           image: Ginger,
-          info: "American Shorthair"
-      },
-      {
-        name: "Jerry",
-        image: Rascal,
-        info: "American Shorthair"
-    },
-    {
-      name: "Jerry",
-      image: Danii,
-      info: "American Shorthair"
-  }
-        ]
+          info: "American Shorthair",
+        },
+        {
+          name: "Jerry",
+          image: Rascal,
+          info: "American Shorthair",
+        },
+        {
+          name: "Jerry",
+          image: Danii,
+          info: "American Shorthair",
+        },
+      ],
     },
 
     {
       name: "Purrrr - Cat friends near you",
       members: [
-          {
-              name: "Benson",
-              image: Finn,
-              info: "...To Be Added"
-          },
-          {
-              name: "Mordecai",
-              image: Asame,
-              info: "...To Be Added"
-          },
-          {
-              name: "Muscle Man",
-              image: Ginger ,
-              info: "...To Be Added"
-          },
-          {
-              name: "Pops",
-              image:Finn,
-              info: "...To Be Added"
-          },
-          {
-              name: "Rigby",
-              image: Asame,
-              info: "...To Be Added"
-          },
-          {
-              name: "Skips",
-              image:Finn ,
-              info: "...To Be Added"
-          }
-          // ... member objects
-          ]
-      },
-
-];
+        {
+          name: "Benson",
+          image: Finn,
+          info: "...To Be Added",
+        },
+        {
+          name: "Mordecai",
+          image: Asame,
+          info: "...To Be Added",
+        },
+        {
+          name: "Muscle Man",
+          image: Ginger,
+          info: "...To Be Added",
+        },
+        {
+          name: "Pops",
+          image: Finn,
+          info: "...To Be Added",
+        },
+        {
+          name: "Rigby",
+          image: Asame,
+          info: "...To Be Added",
+        },
+        {
+          name: "Skips",
+          image: Finn,
+          info: "...To Be Added",
+        },
+        // ... member objects
+      ],
+    },
+  ];
 
   const workInfoDatas = [
     {
@@ -155,8 +153,6 @@ export const Home = () => {
     },
   ];
 
-  
-
   const sliderSettings = {
     dots: true,
     infinite: true,
@@ -164,24 +160,24 @@ export const Home = () => {
     slidesToShow: 5,
     slidesToScroll: 5,
     responsive: [
-    {
+      {
         breakpoint: 1024,
         settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-        }
-    },
-    {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
         breakpoint: 600,
         settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-        }
-    }
-    ]
-};
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   <style>{(document.body.style.backgroundColor = "#E3EAE7")}</style>;
 
   return (
@@ -192,66 +188,70 @@ export const Home = () => {
         </div>
         {/* Sejal's code */}
         <form onSubmit={handleSubmitZipCode} className="zip-form">
-            <input
-                type="text"
-                placeholder="Zip Code"
-                value={zipCode}
-                onChange={handleZipCodeChange}
-                className="zip-input"
-            />
-            <button type="submit" className="zip-submit">Lets Go</button>
+          <input
+            type="text"
+            placeholder="Zip Code"
+            value={zipCode}
+            onChange={handleZipCodeChange}
+            className="zip-input"
+          />
+          <button type="submit" className="zip-submit">
+            Lets Go
+          </button>
         </form>
         {/*  End of Sejal's Code*/}
 
         <div className="home-text-section">
-        <div className="button-container">
-  {/* Navigate to Multifilters page when this button is clicked */}
-  <Link to="/multi-filters" className="third-button">
-  <img src={dogImage} alt="Dog" />
-  <span>Dog</span>
-</Link>
+          <div className="main-container">
+            <div className="button-container">
+              {/* Navigate to Multifilters page when this button is clicked */}
+              <Link to="/multi-filters" className="third-button">
+                <img src={dogImage} alt="Dog" />
+                <span>Dog</span>
+              </Link>
 
-  {/* Navigate to Multifilters page when this button is clicked */}
-  <Link to="/multi-filters" className="third-button">
-  <img src={catImage} alt="Cat" />
-  <span>Cat</span>
-</Link>
+              {/* Navigate to Multifilters page when this button is clicked */}
+              <Link to="/multi-filters" className="third-button">
+                <img src={catImage} alt="Cat" />
+                <span>Cat</span>
+              </Link>
 
-  {/* Navigate to Multifilters page when this button is clicked */}
-  <Link to="/multi-filters" className="third-button">
-  <img src={birdImage} alt="Bird" />
-  <span>Bird</span>
-</Link>
+              {/* Navigate to Multifilters page when this button is clicked */}
+              <Link to="/multi-filters" className="third-button">
+                <img src={birdImage} alt="Bird" />
+                <span>Bird</span>
+              </Link>
 
-  {/* Navigate to Multifilters page when this button is clicked */}
-  <Link to="/multi-filters" className="fifth-button">
-  <img src={bunnyImage} alt="Small Critters" />
-    Small Critters
-  </Link>
-</div>
-    <h1 className="primary-heading">
-            Find your furr-ever friend today!
-          </h1>
-        </div> 
+              {/* Navigate to Multifilters page when this button is clicked */}
+              <Link to="/multi-filters" className="fifth-button">
+                <img src={bunnyImage} alt="Small Critters" />
+                Small Critters
+              </Link>
+            </div>
+            <h1 className="primary-heading">
+              Find your furr-ever friend today!
+            </h1>
+            </div>
+        </div>
       </div>
 
-<div className="about-meet-teamss">
-    <h1>Furr-Ever friends near you</h1>
-    {teams.map((team, index) => (
-        <div key={index} className="about-team-sections">
+      <div className="about-meet-teamss">
+        <h1>Furr-Ever friends near you</h1>
+        {teams.map((team, index) => (
+          <div key={index} className="about-team-sections">
             <h2>{team.name}</h2>
             <Slider {...sliderSettings}>
-                {team.members.map((member, memberIndex) => (
-                    <div key={memberIndex} className="about-team-members">
-                        <img src={member.image} alt={member.name} />
-                        <h3>{member.name}</h3>
-                        <p>{member.info}</p>
-                    </div>
-                ))}
+              {team.members.map((member, memberIndex) => (
+                <div key={memberIndex} className="about-team-members">
+                  <img src={member.image} alt={member.name} />
+                  <h3>{member.name}</h3>
+                  <p>{member.info}</p>
+                </div>
+              ))}
             </Slider>
-        </div>
-    ))}
-</div>
+          </div>
+        ))}
+      </div>
 
       {/* <div className="adoption-faq-container" id="FAQ">
         <h1>Adoption FAQ</h1>
