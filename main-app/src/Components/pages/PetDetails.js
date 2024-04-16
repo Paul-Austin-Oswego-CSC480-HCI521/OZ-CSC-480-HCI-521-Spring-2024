092f7fca16a482b69fbe8fb90b20aedd4cefb1d2
VickramN <vickramshiv2004@gmail.com>
2024-04-16 11:39:32 -0400

    Cleaned up some of the Pet details code to make it easier to work with

--- main-app/src/Components/pages/PetDetails.js
+++ main-app/src/Components/pages/PetDetails.js
@@ -1,23 +1,9 @@
 import React from "react";
 import { FiArrowLeft } from "react-icons/fi";
-import Jax from "../images/Jax.jpeg";
-import Lucy from "../images/lucy4.jpg";
-import Hex from "../images/hex.jpg";
 import ShelterLogo from "../images/Shelterlogo.svg";
-import Watson from "../images/Jax.jpeg";
-import Bubble from "../images/speechbubble.png";
 import "./PetDetails.css";
 import { Link } from "react-router-dom";
 import { IoIosCheckmarkCircle } from "react-icons/io";
-import Luke from "../../Assets/Furr-Ever friends near you_1.png";
-import Finn from "../../Assets/Furr-Ever friends near you_2.png";
-import Brownie from "../../Assets/Furr-Ever friends near you_3.png";
-import Jaxon from "../../Assets/Purrrr - Cat friends near you_1.png";
-import Chloe from "../../Assets/Furr-Ever friends near you_5.png";
-import Asame from "../../Assets/Screenshot2023.png";
-import Ginger from "../../Assets/Screenshot2029.jpg";
-import Rascal from "../../Assets/Screenshot2011.jpg";
-import Danii from "../../Assets/Screenshot2013.JPG";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -37,92 +23,6 @@ export const PetDetails = () => {
     setPet(petDetails);
     setLoading(false);
   }, [id]);
-
-  const pets = [
-    { image: Jax, name: "Jax" },
-    { image: Lucy, name: "Lucy" },
-    { image: Hex, name: "Hex" },
-    { image: Luke, name: "Luke" },
-    { image: Watson, name: "Watson" },
-    // Add more pets as needed
-  ];
-  const teams = [
-    {
-      name: "",
-      members: [
-        {
-          name: "Smoothie, M",
-          image: Luke,
-          info: "American Shorthair",
-        },
-        {
-          name: "Watson, M",
-          image: Finn,
-          info: "American Shorthair",
-        },
-        {
-          name: "Victor, M",
-          image: Brownie,
-          info: "American Shorthair",
-        },
-        {
-          name: "Aofie, F",
-          image: Jaxon,
-          info: "American Shorthair",
-        },
-        {
-          name: "Smoothie F",
-          image: Chloe,
-          info: "American Shorthair",
-        },
-        {
-          name: "Jerry",
-          image: Chloe,
-          info: "American Shorthair",
-        },
-        {
-          name: "Jerry",
-          image: Ginger,
-          info: "American Shorthair",
-        },
-        {
-          name: "Jerry",
-          image: Rascal,
-          info: "American Shorthair",
-        },
-        {
-          name: "Jerry",
-          image: Danii,
-          info: "American Shorthair",
-        },
-      ],
-    },
-  ];
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
 
   if (loading || !pet) {
     return <div>Loading or pet not found...</div>;
@@ -190,7 +90,12 @@ export const PetDetails = () => {
           <div className="column-2">
             <div className="div-5">
               <div className="div-6">
-                <img loading="lazy" srcSet={ShelterLogo} className="img-2" />
+                <img
+                  loading="lazy"
+                  srcSet={ShelterLogo}
+                  className="img-2"
+                  alt=""
+                />
                 <div className="div-7">
                   <div className="div-8">Pulaski County Shelter</div>
                   <i>
