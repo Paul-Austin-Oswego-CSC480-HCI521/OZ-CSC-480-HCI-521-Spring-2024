c3793c2cfd6bbd713ce4d516bb1c153ebf134b8a
Declan Onunkwo <115195431+DeclanGH@users.noreply.github.com>
2024-04-30 12:38:23 -0400

    Update MongoProducer.java to index email field

--- database-controller/src/main/java/oz/rest/MongoProducer.java
+++ database-controller/src/main/java/oz/rest/MongoProducer.java
@@ -2,6 +2,9 @@ package oz.rest;
 
 import java.util.Map;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.IndexOptions;
+import org.bson.Document;
 import org.bson.codecs.configuration.CodecProvider;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
@@ -15,6 +18,9 @@ import com.mongodb.client.MongoDatabase;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Disposes;
 import jakarta.enterprise.inject.Produces;
+import oz.rest.models.Adopter;
+import oz.rest.models.Shelter;
+
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
@@ -42,7 +48,27 @@ public class MongoProducer {
         CodecProvider pojoCodecProvider = fromProviders(PojoCodecProvider.builder().automatic(true).build());
         CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
 
-        return mongoClient.getDatabase("PlaceholderAppName").withCodecRegistry(pojoCodecRegistry);
+        MongoDatabase database = mongoClient.getDatabase("PlaceholderAppName").withCodecRegistry(pojoCodecRegistry);
+
+        // on creating a new db, we would run an initializer
+        initializeCollections(database);
+
+        return database;
+    }
+
+    private void initializeCollections(MongoDatabase database) {
+        createUniqueEmailAdopter(database.getCollection("Adopters", Adopter.class));
+        createUniqueEmailShelter(database.getCollection("Shelters", Shelter.class));
+    }
+
+    private void createUniqueEmailAdopter(MongoCollection<Adopter> adopterCollection) {
+        IndexOptions indexOptions = new IndexOptions().unique(true);
+        adopterCollection.createIndex(new Document("emailAddress", 1), indexOptions);
+    }
+
+    private void createUniqueEmailShelter(MongoCollection<Shelter> shelterCollection) {
+        IndexOptions indexOptions = new IndexOptions().unique(true);
+        shelterCollection.createIndex(new Document("emailAddress", 1), indexOptions);
     }
 
     public void close(@Disposes MongoClient toClose) {
