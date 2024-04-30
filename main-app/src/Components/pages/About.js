91d6f0a409cd33c66d6d0306d5638813add7880d
Mark <markthompson_tla@yahoo.com>
2024-04-30 02:05:20 -0400

    Import statement re-added

--- main-app/src/Components/pages/About.js
+++ main-app/src/Components/pages/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./About.css";
+import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Helmet } from "react-helmet";
