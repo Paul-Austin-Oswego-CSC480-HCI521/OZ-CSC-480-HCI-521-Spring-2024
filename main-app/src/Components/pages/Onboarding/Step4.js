b9bcfd43088bc3e4b270739da455e22ccc9ce744
Henry <henryjrose2@gmail.com>
2024-05-01 09:33:39 -0400

    Shelter form auto generates and sends coordinates to backend for storage

--- main-app/src/Components/pages/Onboarding/Step4.js
+++ main-app/src/Components/pages/Onboarding/Step4.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
-const Step4 = ({ prevStep, formData, step, getProgressBarWidth }) => {
+const Step4 = ({ prevStep, formData }) => {
   const navigate = useNavigate();
 
-  const { name, streetAddress, city, state, zipcode, emailAddress, phoneNumber, description, password } = formData;
+  const {
+    name,
+    streetAddress,
+    city,
+    state,
+    zipcode,
+    emailAddress,
+    phoneNumber,
+    description,
+    password,
+  } = formData;
+  let address = streetAddress + " " + city + " " + state + " " + zipcode;
+  const accessToken =
+    "pk.eyJ1IjoiaGpyb3NlMjkiLCJhIjoiY2x1MGFmbzNmMDJxYTJrbnAyY3J6MWN1NiJ9.T_K7aTjSSiqtAIeRbL5Msw";
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    zipcode +
+    ".json?access_token=" +
+    accessToken;
+  let latitude = 0;
+  let longitude = 0;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      latitude = data.features[0].geometry.coordinates[1];
+      longitude = data.features[0].geometry.coordinates[0];
+    })
+    .catch((error) => {
+      // Handle errors here
+      console.error("Error fetching data:", error);
+    });
 
+  // console.log(url);
+  // console.log(data);
+  // const line = JSON.stringify(data);
+  // console.log(line);
+  // const ind = line.indexOf('coordinates":[');
+  // let coordString = line.substring(ind + 14, ind + 35);
+  // coordString = coordString.substring(0, coordString.indexOf("]"));
+  // let coords = [];
+  // coords[1] = coordString.substring(0, coordString.indexOf(","));
+  // coords[0] = coordString.substring(coordString.indexOf(",") + 1);
+
+  // console.log(coords[0] + ", " + coords[1]);
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -14,19 +55,15 @@ const Step4 = ({ prevStep, formData, step, getProgressBarWidth }) => {
     const formDataToSend = {
       name,
       emailAddress,
-      location: {
-        addressLine1: streetAddress,
-        city,
-        state,
-        zipcode
-      },
-      // streetAddress,
-      // city,
-      // state,
-      // zipcode,
+      streetAddress,
+      city,
+      state,
+      zipcode,
       phoneNumber,
       description,
       password,
+      latitude,
+      longitude,
     };
 
     console.log(formDataToSend);
@@ -34,37 +71,27 @@ const Step4 = ({ prevStep, formData, step, getProgressBarWidth }) => {
     try {
       // TODO: implement actual location
       delete formDataToSend.streetAddress;
-      console.log(formDataToSend);
       // Send the form data as JSON to the endpoint
-      const response = await fetch(process.env.REACT_APP_OPEN_LIBERTY_ROOT + 'database-controller/api/shelter', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formDataToSend),
-      });
+      const response = await fetch(
+        "http://localhost:9080/database-controller/api/shelter",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formDataToSend),
+        }
+      );
 
       if (!response.ok) {
-        console.log(response.status)
-        if (response.status == 409) {
-          alert('The email address entered is already being used, please choose a different one.');
-          // throw new Error('Failed to submit form data');
-        } else {
-          throw new Error('Failed to submit form data');
-        }
+        throw new Error("Failed to submit form data");
       } else {
         //   if (1) {
         // Redirect to another component upon successful login
         const responseJson = await response.json();
         // console.log(responseText);
 
-        const cookieJsonKeys = Object.entries(responseJson)
-
-        for (const entry of cookieJsonKeys) {
-          const suffix = "path=/;";
-          document.cookie = entry[0] + "=" + entry[1] + ";" + suffix;
-        };
-
+        document.cookie = JSON.stringify(responseJson);
 
         console.log(document.cookie);
 
@@ -86,38 +113,59 @@ const Step4 = ({ prevStep, formData, step, getProgressBarWidth }) => {
 
       // alert('Form submitted successfully!');
     } catch (error) {
-      console.error('Error submitting form:', error);
-      alert('An error occurred while submitting the form');
+      console.error("Error submitting form:", error);
+      alert("An error occurred while submitting the form");
     }
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit} className="signup-form">
-        <div className="progress-bar">
-          <div className="progress" style={{ width: getProgressBarWidth() }}></div>
-        </div>
-        <h2>Review Your Information</h2>
-        <p><strong>Shelter Name:</strong> {name}</p>
-        <p><strong>Contact Email:</strong> {emailAddress}</p>
-        <p><strong>Address:</strong> {streetAddress}</p>
-        <p><strong>City:</strong> {city}</p>
-        <p><strong>State:</strong> {state}</p>
-        <p><strong>Zipcode:</strong> {zipcode}</p>
-        <p><strong>Contact Phone:</strong> {phoneNumber}</p>
-        <p><strong>Shelter Description:</strong> {description}</p>
+    <form onSubmit={handleSubmit} className="signup-form">
+      <h2>Review Your Information</h2>
+      <p>
+        <strong>Shelter Name:</strong> {name}
+      </p>
+      <p>
+        <strong>Contact Email:</strong> {emailAddress}
+      </p>
+      <p>
+        <strong>Address:</strong> {streetAddress}
+      </p>
+      <p>
+        <strong>City:</strong> {city}
+      </p>
+      <p>
+        <strong>State:</strong> {state}
+      </p>
+      <p>
+        <strong>Zipcode:</strong> {zipcode}
+      </p>
+      <p>
+        <strong>Contact Phone:</strong> {phoneNumber}
+      </p>
+      <p>
+        <strong>Shelter Description:</strong> {description}
+      </p>
 
-        <div className="signup-buttons-container">
-          <button onClick={prevStep}>Back</button>
-          <button type="submit">
-            Finish
-            <svg class="checkmark" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
-              <path d="M9.54961 18L3.84961 12.3L5.27461 10.875L9.54961 15.15L18.7246 5.97498L20.1496 7.39998L9.54961 18Z" fill="currentColor" />
-            </svg>
-          </button>
-        </div>
-      </form>
-    </>
+      <div className="signup-buttons-container">
+        <button onClick={prevStep}>Back</button>
+        <button type="submit">
+          Finish
+          <svg
+            class="checkmark"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+          >
+            <path
+              d="M9.54961 18L3.84961 12.3L5.27461 10.875L9.54961 15.15L18.7246 5.97498L20.1496 7.39998L9.54961 18Z"
+              fill="currentColor"
+            />
+          </svg>
+        </button>
+      </div>
+    </form>
   );
 };
 
